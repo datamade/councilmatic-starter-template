a4097f61dc744574208afb6f0de602d75479b6a3
Cathy Deng <cathyd125@gmail.com>
2016-02-23 13:53:28 -0600

    clean starter app

---
+++ yourcity/models.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.db import models
+from councilmatic_core.models import Bill, Event
+from datetime import datetime
+import pytz
+
+app_timezone = pytz.timezone(settings.TIME_ZONE)
---
+++ yourcity/views.py
@@ -0,0 +1,4 @@
+from councilmatic_core.models import *
+from councilmatic_core.views import *
+from django.conf import settings
+from django.shortcuts import render
